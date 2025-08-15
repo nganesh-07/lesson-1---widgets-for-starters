2d977d1b3b3bfb7d8ff473223182429d8515079c
nganesh <nganesh@iu.edu>
2025-08-14 19:33:16 -0700

    pygame window

---
+++ pygame window.py
@@ -0,0 +1,10 @@
+# import libraries needed
+from tkinter import *
+
+# create window
+window = Tk()
+
+# set window title and geometry
+window.title("demo window")
+window.geometry("400x300")
+window.mainloop()
